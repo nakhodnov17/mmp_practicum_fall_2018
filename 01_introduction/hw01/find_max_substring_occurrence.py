e139dfa7c239d8ca40a2139ed92b67310629a188
nakhodnov17 <nakhodnov17@gmail.com>
2018-09-22 14:30:43 +0300

    delete files for future encryption

--- 01_introduction/hw01/find_max_substring_occurrence.py
+++
@@ -1,4 +0,0 @@
-def find_max_substring_occurrence(input_string):
-    for idx in range(1, len(input_string) + 1):
-        if input_string[:idx] * (len(input_string) // idx) == input_string:
-            return len(input_string) // idx
--- 01_introduction/hw01/subreverse_sequence.py
+++
@@ -1,2 +0,0 @@
-def subreverse_sequence(sequence):
-    return tuple(sequence[::2][::-1] + sequence[1::2])
