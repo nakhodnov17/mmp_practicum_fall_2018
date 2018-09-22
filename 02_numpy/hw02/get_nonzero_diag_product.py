d042cc5f05bffdf15d63b27bc8a2304823200809
nakhodnov17 <nakhodnov17@gmail.com>
2018-09-22 14:30:55 +0300

    delete files for future encryption

--- 02_numpy/hw02/get_nonzero_diag_product.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def get_nonzero_diag_product(x):
-    diag = np.diag(x)
-    non_zero_diag = diag[diag != 0]
-    if non_zero_diag.shape[0] == 0:
-        return None
-    return np.prod(non_zero_diag)
--- 02_numpy/hw02/replace_nan_to_means.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def replace_nan_to_means(x):
-    x = np.copy(x)
-    x[:, np.count_nonzero(np.isnan(x), axis=0) == x.shape[1]] = 0.
-    means = np.nanmean(x, axis=0).reshape([1, -1])
-    x[np.isnan(x)] = np.repeat(means, x.shape[0], axis=0)[np.isnan(x)]
-    return x
